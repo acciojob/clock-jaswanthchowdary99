fa38fb8c57a0d323938244df008b83b9bd47b27c
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-11-20 14:58:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let timerDisply = document.getElementById("timer");
+let currentDate = new Date();
+
+setInterval(
+	function(){
+		currentDate = new Date();
+		timerDisply.innerHTML = currentDate.toLocaleString();
+	},1000
+)
